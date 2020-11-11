952ff3b3ab4c695a94713d49ed09b5e545f9fbde
rotemha <rotemhas@post.bgu.ac.il>
2020-11-11 11:47:41 +0200

    lecture 181 - creating an order in firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -77,6 +78,23 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue!');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Rotem Hashahar',
+                address:{
+                    street: 'Ginosar',
+                    zipCode: '1234',
+                    country: 'Israel'
+                },
+                email: 'rotem@rotem.com'
+            },
+           deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
     }
 
     render () {
